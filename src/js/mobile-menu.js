f5c3fadee5d517b936af3e28ebf386e24511aad8
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2023-02-19 19:10:51 +0200

    fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -9,21 +9,14 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    // const scrollLockMethod = !isMenuOpen
-    //   ? 'disableBodyScroll'
-    //   : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 1000px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
